09f31174030cad3c0474b6a0a969e0f93ff8ddee
Ricardo Carvalho <ricardo0726.es@gmail.com>
2026-07-16 19:34:26 -0300

    Organização e limpeza de arquivos padrões

--- app/src/main/java/com/ricardocarvalho/mylibrary/ui/HomeFragment.kt
+++ app/src/main/java/com/ricardocarvalho/mylibrary/ui/HomeFragment.kt
@@ -1,4 +1,4 @@
-package com.ricardocarvalho.mylibrary.ui.home
+package com.ricardocarvalho.mylibrary.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -6,33 +6,26 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.ricardocarvalho.mylibrary.databinding.FragmentHomeBinding
+import com.ricardocarvalho.mylibrary.viewmodels.HomeViewModel
 
 class HomeFragment : Fragment() {
 
     private var _binding: FragmentHomeBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private val viewModel: HomeViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/ricardocarvalho/mylibrary/ui/MainActivity.kt
+++ app/src/main/java/com/ricardocarvalho/mylibrary/ui/MainActivity.kt
@@ -1,12 +1,16 @@
-package com.ricardocarvalho.mylibrary
+package com.ricardocarvalho.mylibrary.ui
 
 import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.ricardocarvalho.mylibrary.R
 import com.ricardocarvalho.mylibrary.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -15,19 +19,28 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        supportActionBar?.hide()
+
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setUpNavigation()
+    }
+
+    private fun setUpNavigation() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
+            setOf(R.id.navigation_home, R.id.navigation_favorite)
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
